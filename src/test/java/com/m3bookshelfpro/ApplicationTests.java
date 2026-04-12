a2177099daa3f704ef07d02452fca2b43c1f5af3
JJ <jisrael1@my.gcu.edu>
2026-04-12 02:12:36 -0700

    Update ApplicationTests.java

--- src/test/java/com/m3bookshelfpro/ApplicationTests.java
+++ src/test/java/com/m3bookshelfpro/ApplicationTests.java
@@ -1,13 +1,9 @@
 package com.m3bookshelfpro;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
