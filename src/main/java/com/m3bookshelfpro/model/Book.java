d18098ac5d5fa7f62fbffd840faf16c8bfa6bbcd
Peyton Wolf <peytonwolf1234@gmail.com>
2026-04-19 23:58:46 -0400

    fix CI/CD pipeline

--- src/main/java/com/m3bookshelfpro/model/Book.java
+++ src/main/java/com/m3bookshelfpro/model/Book.java
@@ -20,4 +20,17 @@ public class Book {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+    
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
--- src/main/java/com/m3bookshelfpro/model/Category.java
+++ src/main/java/com/m3bookshelfpro/model/Category.java
@@ -12,4 +12,18 @@ public class Category {
     private Long id;
     private String name;
     private String description;
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
